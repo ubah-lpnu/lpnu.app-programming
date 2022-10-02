482b3905ff7f5f1bececea098e902baff644e5aa
bohdants96 <bohdantsisinskyi@gmail.com>
2022-10-03 00:10:06 +0300

    Notes API

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from blueprint import api_blueprint
 
 app = Flask(__name__)
 
-with make_server('',5000, app) as server:
-    app.register_blueprint(api_blueprint,url_prefix='/api/v1')
+with make_server('', 5000, app) as server:
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
     server.serve_forever()
 
 # curl -v -XGET http://localhost:5000/api/v1/hello-world
