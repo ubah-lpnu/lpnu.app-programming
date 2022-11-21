c065f5bc587b970e6c77ac73d115fc6be4f2c6d0
MaksShu <54746440+MaksShu@users.noreply.github.com>
2022-11-21 11:29:21 +0200

    [lab9] gitignore

--- alembic/env.py
+++ alembic/env.py
@@ -18,7 +18,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-import model_objects
+from to_test import model_objects
+
 target_metadata = model_objects.BaseModel.metadata
 
 # other values from the config, defined by the needs of env.py,
